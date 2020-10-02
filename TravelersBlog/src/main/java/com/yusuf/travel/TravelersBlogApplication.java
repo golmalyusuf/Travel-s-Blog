47a7bfcd061c033fde972d46466291f9411ddb4a
Yusuf <common.dsdl@gmail.com>
2020-10-03 00:36:36 +0600

    Created travel blog project and pushing it in the git.

---
+++ TravelersBlog/src/main/java/com/yusuf/travel/TravelersBlogApplication.java
@@ -0,0 +1,12 @@
+package com.yusuf.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelersBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelersBlogApplication.class, args);
+	}
+}
---
+++ TravelersBlog/src/test/java/io/javabrains/TravelersBlogApplicationTests.java
@@ -0,0 +1,9 @@
+package io.javabrains;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelersBlogApplicationTests {
+
+
+}
